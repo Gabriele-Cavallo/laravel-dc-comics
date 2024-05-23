503b5289f8b6b17f9f69cd5b77c528e9d50885fa
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-05-23 14:36:03 +0200

    feat: add index view

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>ciao siamo i comics</h1>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="container">
+        <nav class="nav-wrapper">
+            <ul>
+                <li>
+                    <a href="{{ route('home')}}">HOME</a>
+                </li>
+                <li>
+                    <a href="{{ route('comics')}}">COMICS</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
